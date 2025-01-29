05b92617e573f4b1e9a07f9956407240262498b6
Gustavo Paulino <Guh.Naasc@hotmail.com>
2025-01-29 00:26:09 -0300

    Ajustando a dag

---
+++ dags/minha_primieira_dag.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+        dag_id='minha primeira dag',
+        description='ETl do pai',
+        schedule="* * * * *",
+        start_date= datetime(2025,1,27),
+        catchup=False # backfill
+)
+
+def pipeline(): 
+    @task
+    def primeira_atividade():
+        print("Atividade 1")
+        sleep(2)
+    @task
+    def segunda_atividade():
+        print("Atividade 2")
+        sleep(2)
+    @task
+    def terceira_atividade():
+        print("Atividade 3")
+        sleep(2)    
+    
+    t1 = primeira_atividade()
+    t2 = segunda_atividade()
+    t3 = terceira_atividade()
+
+    t1 >> t2 >> t3
+    
