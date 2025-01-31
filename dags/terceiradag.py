1952d1105ea0cdbd1f000d2ce8fb28d8b3ad51fa
Gustavo Paulino <Guh.Naasc@hotmail.com>
2025-01-31 19:22:27 -0300

    Atualizando o airflow com mais dags

---
+++ dags/terceiradag.py
@@ -0,0 +1,44 @@
+from time import sleep
+
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+        dag_id='terceiradag',
+        description='ETl do pai',
+        schedule="* * * * *",
+        start_date= datetime(2025,1,27),
+        catchup=False # backfill
+)
+
+def pipeline(): 
+    @task
+    def primeira_atividade():
+        print("Atividade 1")
+        sleep(2)
+
+    @task
+    def segunda_atividade():
+        print("Atividade 2")
+        sleep(2)
+        
+    @task
+    def terceira_atividade():
+        print("Atividade 3")
+        sleep(2)
+
+    @task
+    def quarta_atividade():
+        print("Atividade 4")
+        sleep(2)    
+    
+    t1 = primeira_atividade()
+    t2 = segunda_atividade()
+    t3 = terceira_atividade()
+    t4 = quarta_atividade()
+
+    t1 >> [t2,t3]
+    t3 << t4
+
+pipeline()
+    
